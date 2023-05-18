0f3eba338b2b02728316d8be8026e89308d1f1f1
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-05-19 04:22:14 +0900

    🗒️ コメントを追記しました。

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 
 import { createApp, ref, computed } from "https://unpkg.com/vue@3.2.4/dist/vue.esm-browser.prod.js";
+
+// ==v-on:focus========================================================================
+// v-on:focusまたは@focus:フォーカスされた時のイベント
+// @focus="ageValidationVisible = false"
+// ユーザーが入力フィールドを選択して編集し始めると、
+// ageValidationVisibleがfalseに設定されます。これにより、バリデーションメッセージが非表示になります。
+// ============================================================================
+
+// ==v-on:blur========================================================================
+// v-on:blurまたは@blur:フォーカスが外れた時のイベント
+// ageValidationVisibleが!age（ageが空でない場合はfalse、空の場合はtrue）に設定されます。
+// ============================================================================
+
+// ==v-model========================================================================
+// v-model="age"は、入力フィールドとageというデータプロパティをバインディングしています。
+// 入力フィールドに入力された値は、ageに自動的に反映されます。
+// ============================================================================
+
 const app = createApp({
   setup() {
     const name = ref("");
@@ -9,6 +27,11 @@ const app = createApp({
     const ageValidationVisible = ref(false);
     const emailValidationVisible = ref(false);
 
+    // ============================================================================
+    // !<hoge>.value
+    // 論理否定演算子で各データの値を反転させ、
+    // 空文字列であるかどうかを判定しています
+    // ============================================================================
     const validateForm = () => {
       if (!name.value) {
         nameValidationVisible.value = true;
@@ -23,6 +46,12 @@ const app = createApp({
         return;
       }
     };
+
+    // ==computed========================================================================
+    // computedは算出プロパティ
+    // 各プロパティひとつでも空文字であればtrueを返します。
+    // :disabled="isButtonDisabled"は初期値false
+    // ============================================================================
 
     const isButtonDisabled = computed(() => {
       return !name.value || !age.value || !email.value;
